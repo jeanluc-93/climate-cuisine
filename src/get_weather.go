a30eb68f7a5efae711a83eae24839555a254ebaa
Jean-Luc <123123841+jeanluc-93@users.noreply.github.com>
2023-08-16 23:57:45 +0200

    Started working on the lambda.

--- src/get_weather.go
+++ src/get_weather.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func LambdaHandler() (string, error) {
+	return fmt.Sprintf("Hello, %v", "From AWS lambda."), nil
+}
+
+func main() {
+	lambda.Start(LambdaHandler)
 }
