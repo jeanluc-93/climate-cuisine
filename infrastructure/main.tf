0965f666abb167adf0556b91f930447a0be57961
Jean-Luc <123123841+jeanluc-93@users.noreply.github.com>
2023-08-17 12:57:48 +0200

    Updated required provider statement

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.12.0"
     }
   }
-
-  required_version = "~> 1.0"
 }
 
 # Configure the AWS Provider
