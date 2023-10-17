e1b774592d236b077d92722d0842284179ef7641
Jean-Luc <jeanluc@localhost.localdomain>
2023-10-17 22:26:27 +0200

    Terraform reset

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,32 +1,32 @@
-variable "default_region" {
-    description = "Cape Town region"
-    default = "af-south-1"
-    type = string
-}
+# variable "default_region" {
+#     description = "Cape Town region"
+#     default = "af-south-1"
+#     type = string
+# }
 
-// Variables saved in Terraform cloud workspace - loaded here.
-variable "openWeatherMap_url_key" {
-    description = "Reference key to load the Open Weather Map url from Parameter store."
-    type = string
-}
+# // Variables saved in Terraform cloud workspace - loaded here.
+# variable "openWeatherMap_url_key" {
+#     description = "Reference key to load the Open Weather Map url from Parameter store."
+#     type = string
+# }
 
-variable "openWeatherMap_url_value" {
-    description = "URL for the Open Weather Map api."
-    type = string
-}
+# variable "openWeatherMap_url_value" {
+#     description = "URL for the Open Weather Map api."
+#     type = string
+# }
 
-variable "openWeatherMap_apiKey_key" {
-    description = "Key that is used to access the correlating value from secrets manager."
-    type = string
-}
+# variable "openWeatherMap_apiKey_key" {
+#     description = "Key that is used to access the correlating value from secrets manager."
+#     type = string
+# }
 
-variable "openWeatherMap_apiKey_value" {
-    description = "The API Key to access Open Weather Map."
-    type = string
-    sensitive = true
-}
+# variable "openWeatherMap_apiKey_value" {
+#     description = "The API Key to access Open Weather Map."
+#     type = string
+#     sensitive = true
+# }
 
-variable "sqs_queue_get_dinner" {
-    description = "SQS queue used to push messages to additional lambda to get dinner ideas based off of the received input."
-    type = string
-}
+# variable "sqs_queue_get_dinner" {
+#     description = "SQS queue used to push messages to additional lambda to get dinner ideas based off of the received input."
+#     type = string
+# }
